a1f06f835a01ee2cc656e4be9a7fafd5cce6a36f
Miygteet <148593184+Miygteet@users.noreply.github.com>
2025-06-05 09:49:15 -0400

    Update PORT.js

--- PORT.js
+++ PORT.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 5268
+const port = process.env.PORT || 5000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
